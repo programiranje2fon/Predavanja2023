9ce340506c09f5d53f5a03da79806598e73a9d04
bojantomic <tomicb@fon.rs>
2023-10-16 11:56:32 +0200

    Dodata resenja dvocas 3 ponedeljak

---
+++ src/dvocas3_ponedeljak/zadatak1/TestIspisivac.java
@@ -0,0 +1,25 @@
+package dvocas3_ponedeljak.zadatak1;
+
+public class TestIspisivac {
+
+	public static void main(String[] args) {
+		//Ispisivac is = new Ispisivac();
+		
+		/*Ispisivac.ispisiOcenu(3);
+		Ispisivac.ispisiOcenu(1);
+		Ispisivac.ispisiOcenu(786);
+		
+		Ispisivac.ispisiZdravo();
+		
+		System.out.println("Min ocena je: " + Ispisivac.MIN_OCENA);
+		
+		Ispisivac.ispisiPoruku("16.10.2023. 12C");
+		
+		Ispisivac.ispisiPoruku("Poruka broj 2", 3);*/
+		
+		//Ispisivac.ispisiBrojeveOd1Do20();
+		
+		Ispisivac.vremeDupliranja(1000, 0.15);
+	}
+
+}
