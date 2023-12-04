caa35e4f0fafa5089ea2d9bee77defa6566148c4
bojantomic <tomicb@fon.rs>
2023-12-04 11:55:07 +0100

    Dodata resenja za dvocas 10 ponedeljak

---
+++ src/dvocas10_ponedeljak/zadatak2/test/TestImenik.java
@@ -0,0 +1,18 @@
+package dvocas10_ponedeljak.zadatak2.test;
+
+import dvocas10_ponedeljak.zadatak2.Imenik;
+
+public class TestImenik {
+
+	public static void main(String[] args) {
+		Imenik im = new Imenik();
+		
+		im.ucitajOsobu();
+		
+		im.ispisi();
+		
+		im.pronadjiOsobu();
+
+	}
+
+}
